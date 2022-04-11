0b3885fe27c0246a7313fc1e1a7cb8406bdedb51
MagdK <kovacsmagda@gmail.com>
2022-04-11 17:22:52 +0200

    Inheritance, destructuring, lovesem sincs

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import {Component} from "react"
+
+class Header extends Component{
+    render() {
+        return (
+            <header>{this.props.title}</header>
+        )
+    }
+}
+
+export default Header
+
+/* 
+Oroklodes - INHERITANCE?
+
+
+Styled-component: 
+https://styled-components.com/
+
+Css-t be tudjuk irni  a componenst faljba. Ez sem js-sel hozzaibiggyesztett css lesz, hanem szep korrekt.
+ha ezt nem csinaljunk, csinaljunk egy beer mappat, legyen azon belul egy beer.jsx, beer.css
+
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Beers from './components/Beers'
-import data from './data.json'
+import Beers from './components/Beers';
+import data from './data.json';
+import Header from './components/Header';
 
 // console.log(data.logo)
 // console.log(data.cards)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header title={data.logo}/>
     <Beers list={data.cards} />
   </React.StrictMode>,
   document.getElementById('root')
